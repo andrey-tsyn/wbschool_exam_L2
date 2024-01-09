d23b179c7cad337e5c6ac80b3d9b1dba4163a870
Andrey <andrej.tsyn@yandex.ru>
2024-01-09 22:33:24 +0300

    add 2nd task solution

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+var (
+	IncorrectString = errors.New("provided string is incorrect")
+)
+
 /*
 === Задача на распаковку ===
 
@@ -18,6 +29,61 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func UnpackString(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
 
+	runes := []rune(str)
+
+	if unicode.IsDigit(runes[0]) {
+		return "", IncorrectString
+	}
+
+	builder := strings.Builder{}
+	runesLen := len(runes)
+	backSlashRepeated := false
+
+	for i := 0; i < runesLen; i++ {
+		symbol := runes[i]
+
+		// Проверка на обратный слеш, если он есть, то переключаемся на следующий символ
+		if symbol == '\\' && !backSlashRepeated {
+			i++
+			if i == runesLen {
+				return "", IncorrectString
+			}
+			symbol = runes[i]
+			// Если следующий символ также обратный слеш, то мы ставим флаг, чтобы при следующей операции работать с
+			// ним, как с обычным
+			if symbol == '\\' {
+				backSlashRepeated = true
+			}
+		} else {
+			backSlashRepeated = false
+		}
+
+		// Получаем длину последовательности цифр(если есть)
+		startDigitsIndex, endDigitsIndex := i+1, 0
+		for i+1 != runesLen && unicode.IsDigit(runes[i+1]) {
+			i++
+			endDigitsIndex = i
+		}
+
+		// Если цифры были найдены, то парсим их в int и добавляем строку с повторяющимся символом заданное
+		// кол-во раз
+		if endDigitsIndex != 0 {
+			count, err := strconv.Atoi(string(runes[startDigitsIndex : endDigitsIndex+1]))
+			if err != nil {
+				// Паника, так как этого не должно произойти в программе, это не ошибка пользователя
+				panic("Function must provide correct rune array with only digits!")
+			}
+			builder.WriteString(strings.Repeat(string(symbol), count))
+			continue
+		}
+
+		builder.WriteRune(symbol)
+	}
+
+	return builder.String(), nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,87 @@
 package main
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestUnpackString(t *testing.T) {
+	tests := []struct {
+		name           string
+		input          string
+		exceptedResult string
+		exceptedError  error
+	}{
+		{
+			name:           "Empty string",
+			input:          "",
+			exceptedResult: "",
+			exceptedError:  nil,
+		},
+		{
+			name:           "One character",
+			input:          "c",
+			exceptedResult: "c",
+			exceptedError:  nil,
+		},
+		{
+			name:           "Digits after char",
+			input:          "c4",
+			exceptedResult: "cccc",
+			exceptedError:  nil,
+		},
+		{
+			name:           "Digits after char several times",
+			input:          "a1b2c3d4e5fff",
+			exceptedResult: "abbcccddddeeeeefff",
+			exceptedError:  nil,
+		},
+		{
+			name:           "With escape sequence",
+			input:          "qwe\\4\\5",
+			exceptedResult: "qwe45",
+			exceptedError:  nil,
+		},
+		{
+			name:           "With escape sequence 2",
+			input:          "qwe\\45",
+			exceptedResult: "qwe44444",
+			exceptedError:  nil,
+		},
+		{
+			name:           "With escape sequence 3",
+			input:          "qwe\\\\5",
+			exceptedResult: "qwe\\\\\\\\\\",
+			exceptedError:  nil,
+		},
+		{
+			name:           "Starts with digit",
+			input:          "32k",
+			exceptedResult: "",
+			exceptedError:  IncorrectString,
+		},
+		{
+			name:           "Digits only",
+			input:          "32461237",
+			exceptedResult: "",
+			exceptedError:  IncorrectString,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result, err := UnpackString(test.input)
+			if err != nil {
+				if errors.Is(err, test.exceptedError) {
+					return
+				}
+				t.Errorf("unexpected error recieved")
+				return
+			}
+
+			if result != test.exceptedResult {
+				t.Errorf("unexpected result recieved.")
+			}
+		})
+	}
+}
